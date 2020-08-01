1089c8b7555746f216df04ff0dcbf35a9eef8c37
Timothy R. Bula <trbula@us.ibm.com>
2020-07-31 20:10:59 -0400

    fix: storybook fonts

--- packages/carbon-addons-boomerang-react/.storybook/preview.js
+++ packages/carbon-addons-boomerang-react/.storybook/preview.js
@@ -1,7 +1,6 @@
 import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
-import "typeface-ibm-plex-sans";
 
 addDecorator(withKnobs);
 addDecorator(withInfo);
