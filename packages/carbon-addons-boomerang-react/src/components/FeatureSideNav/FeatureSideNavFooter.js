c782d0c67ba4da6b23e7d3e4f70b9df7e193e8d8
Isabela Kawabata <isabela_mkb@hotmail.com>
2020-08-31 12:23:50 -0300

    Feature plt 4621 feature sidenav (#19)
    * Add FeaturesSideNav and MemberBar
    
    * fix: remove member bar - added in other branch
    
    * Updates for FeatureSideNav
    
    * Add test specs
    
    * feat: feature sidenav - Add skeleton loading
    
    Co-authored-by: Isabela Kawabata <isabela.kawabata@ibm.com>

--- packages/carbon-addons-boomerang-react/src/components/FeatureSideNav/FeatureSideNavFooter.js
+++ packages/carbon-addons-boomerang-react/src/components/FeatureSideNav/FeatureSideNavFooter.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import { SkeletonPlaceholder } from 'carbon-components-react';
 import { settings } from 'carbon-components';
 
 FeatureSideNavFooter.propTypes = {
   children: PropTypes.any,
   className: PropTypes.string,
+  isLoading: PropTypes.bool,
 };
 
 const { prefix } = settings;
 
 export function FeatureSideNavFooter(props) {
-  const { children, className, ...rest } = props;
+  const { children, className, isLoading, ...rest } = props;
   return (
     <section className={cx(`${prefix}--bmrg-feature-sidenav-footer`, className)} {...rest}>
-      {children}
+      {isLoading ? (
+        <SkeletonPlaceholder style={{ padding: '1rem', height: '3rem', width: '100%' }} />
+      ) : (
+        children
+      )}
     </section>
   );
 }
