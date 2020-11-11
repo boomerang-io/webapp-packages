ea06f78ef41cdb52586075edf992c516d0cfbc69
Tim Bula <timrbula@gmail.com>
2020-11-11 18:54:54 -0500

    Update userProfile.js

--- packages/cra-template-boomerang/template/src/ApiServer/fixtures/userProfile.js
+++ packages/cra-template-boomerang/template/src/ApiServer/fixtures/userProfile.js
@@ -1,16 +1,18 @@
-export default {
-  "id": "59aebdec742453efce952Dde",
-  "email": "johndoe@ibm.com",
-  "name": "John Doe",
-  "isFirstVisit": false,
-  "type": "admin",
-  "hasConsented": true,
-  "isShowHelp": true,
-  "firstLoginDate": 1504649578000,
-  "lastLoginDate": 1525885894391,
-  "lowerLevelGroupIds": [],
-  "pinnedToolIds": [],
-  "favoritePackages": [],
-  "personalizations": {},
-  "notificationSettings": {}
+const userProfile = {
+  id: "59aebdec742453efce952Dde",
+  email: "johndoe@ibm.com",
+  name: "John Doe",
+  isFirstVisit: false,
+  type: "admin",
+  hasConsented: true,
+  isShowHelp: true,
+  firstLoginDate: 1504649578000,
+  lastLoginDate: 1525885894391,
+  lowerLevelGroupIds: [],
+  pinnedToolIds: [],
+  favoritePackages: [],
+  personalizations: {},
+  notificationSettings: {},
 };
+
+export default userProfile;
