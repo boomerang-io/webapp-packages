a7ab855fee450b12ddb317209f3a9cd373eae805
Tim Bula <timrbula@gmail.com>
2020-11-11 18:54:37 -0500

    Update navigation.js

--- packages/cra-template-boomerang/template/src/ApiServer/fixtures/navigation.js
+++ packages/cra-template-boomerang/template/src/ApiServer/fixtures/navigation.js
@@ -1,21 +1,23 @@
-export default {
-  "navigation": [
+const navigation = {
+  navigation: [
     { name: "Launchpad", url: "https://launch.boomerangplatform.net/launchpad" },
     { name: "Catalog", url: "https://launch.boomerangplatform.net/catalog" },
     { name: "Status", url: "https://launch.boomerangplatform.net/status" },
     { name: "Docs", url: "https://launch.boomerangplatform.net/docs" },
-    { name: "Admin", url: "https://launch.boomerangplatform.net/admin" }
+    { name: "Admin", url: "https://launch.boomerangplatform.net/admin" },
   ],
-  "features": {
-    "notifications.enabled": false
+  features: {
+    "notifications.enabled": false,
   },
-  "platform": {
-    "name": "IBM Boomerang Platform",
-    "version": "5.0.0",
-    "signOutUrl": "ibm.com",
-    "platformName": "Boomerang",
-    "displayLogo": true,
-    "privateTeams": false,
-    "sendMail": true
-  }
+  platform: {
+    name: "IBM Boomerang Platform",
+    version: "5.0.0",
+    signOutUrl: "ibm.com",
+    platformName: "Boomerang",
+    displayLogo: true,
+    privateTeams: false,
+    sendMail: true,
+  },
 };
+
+export default navigation;
