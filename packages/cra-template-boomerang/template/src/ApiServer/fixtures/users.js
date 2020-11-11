32e7aa526a245dc54028390f5ef8a759c253dea5
Tim Bula <timrbula@gmail.com>
2020-11-11 18:55:22 -0500

    Update users.js

--- packages/cra-template-boomerang/template/src/ApiServer/fixtures/users.js
+++ packages/cra-template-boomerang/template/src/ApiServer/fixtures/users.js
@@ -1,5 +1,7 @@
-export default [
+const users = [
   { id: 1, name: "Boomerang Jane", email: "bmrgjane@ibm.com" },
   { id: 2, name: "Boomerang Joe", email: "bmrgjoe@ibm.com" },
   { id: 3, name: "Boomerang Ada", email: "bmrgada@ibm.com" },
 ];
+
+export default users;
