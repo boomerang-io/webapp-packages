7003dceac208c5dc2b4935ecb340041d19014d75
Isabela Mayumi Kawabata Borges <isabela.kawabata@ibm.com>
2024-07-29 10:39:13 -0300

    test: add product code data

--- packages/webapp-spa-server/index.js
+++ packages/webapp-spa-server/index.js
@@ -204,6 +204,8 @@ function getBluemixSegmentScripts() {
           page: {
             pageInfo: {
               pageID: "ibm_consulting_advantage_test",
+              productCode: "694970X",
+              productCodeType: "Consulting Assistants",
               productTitle: "IBM Consulting Advantage",
               analytics: {
                 category: "Offering Interface"
