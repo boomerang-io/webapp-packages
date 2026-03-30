85ad21a3f427487298e4fc5c095064c3619837af
Lucas Grimauth Evangelista <lucas.grimauth@Lucass-MacBook-Pro.local>
2026-03-30 16:11:33 -0300

    fix: express v5 migration match all paths including root

--- packages/webapp-spa-server/index.js
+++ packages/webapp-spa-server/index.js
@@ -97,7 +97,7 @@ function createBoomerangServer({
         index: false,
       })
     );
-    appRouter.get("/*path", (_, res) =>
+    appRouter.get("/{*path}", (_, res) =>
       injectEnvDataAndScriptsIntoHTML({
         res,
         appRoot: APP_ROOT,
